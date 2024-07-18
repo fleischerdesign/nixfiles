0f732eb530bc1b3337b2007e79f4dd9c8d8b3081
fleischerdesign <philipp@fleischer.design>
2024-07-18 06:21:44 +0200

    added hosts

--- flake.nix
+++ flake.nix
@@ -1,29 +1,44 @@
-{
-  description = "NixOS configuration";
+# SPDX-License-Identifier: MIT
 
+{
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager?ref=release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager-unstable.url = "github:nix-community/home-manager";
+    home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      Marx = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.philipp = import ./home.nix;
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, home-manager-unstable, sops-nix, ... }@inputs:
+    {
+      nixosConfigurations = {
+        yorke = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/yorke/configuration.nix
+            home-manager.nixosModules.home-manager
+          ];
+        };
 
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
+        jello = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/jello/configuration.nix
+            home-manager.nixosModules.home-manager
+          ];
+        };
+      };
+
+      hmModules = {
+        philipp = {
+          imports = [ ./home-manager/philipp/home.nix ];
+        };
+        server-admin = {
+          imports = [ ./home-manager/server-admin/home.nix ];
+        };
       };
     };
-  };
 }
