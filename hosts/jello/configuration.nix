c71e227cfb587114d5aeba5eca9214e8556c9724
fleischerdesign <philipp@fleischer.design>
2024-07-21 23:23:27 +0200

    refactoring

--- hosts/jello/configuration.nix
+++ hosts/jello/configuration.nix
@@ -1,27 +1,16 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, inputs, nix-vscode-extensions, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    [ ./hardware-configuration.nix
       ../default.nix
     ];
 
-  networking.hostName = "jello"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Define the hosstname
+  networking.hostName = "jello";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable the X11 windowing system.
+  # Enable the X11 windowing system and desktop environment
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm = {
     enable = true;
     wayland = true;
@@ -29,12 +18,8 @@
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "de";
-    xkb.variant = "";
-  };
-
-  # Enable CUPS to print documents.
+  services.xserver.xkb.layout = "de";
+  # Enable CUPS to print documents
   services.printing.enable = true;
 
   # Enable sound with pipewire.
@@ -45,45 +30,32 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # Uncomment below for JACK applications
+    # jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
+  # Home Manager settings
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-
   home-manager.users.philipp = inputs.self.hmModules.philipp;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-
-  programs.adb.enable = true;
-
+  # Define the user account
   users.users.philipp = {
     isNormalUser = true;
     description = "Philipp Fleischer";
     extraGroups = [ "networkmanager" "wheel" "adbusers" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-
+  # Enable ADB and Steam with firewall settings
+  programs.adb.enable = true;
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
   };
 
+  # Exclude unnecessary GNOME packages, maintain essential ones
   environment.gnome.excludePackages = (with pkgs; [
     gnome-photos
     gnome-tour
@@ -105,38 +77,15 @@
     gnome-weather
     gnome-maps
     gnome-clocks
-]);
+  ]);
 
   services.xserver.excludePackages = [ pkgs.xterm ];
 
+  # Enable KDE Connect with GSConnect package
     programs.kdeconnect.enable = true;
     programs.kdeconnect.package = pkgs.gnomeExtensions.gsconnect;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  # State version setting
+  system.stateVersion = "24.05"; # Keep this to match your initial install
 
 }
