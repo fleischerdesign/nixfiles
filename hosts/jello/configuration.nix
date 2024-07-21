0a2d525aeb1b43cad91a7c88778e18fb2c86f48a
fleischerdesign <philipp@fleischer.design>
2024-07-21 23:43:17 +0200

    created gnome.nix

--- hosts/jello/configuration.nix
+++ hosts/jello/configuration.nix
@@ -5,6 +5,7 @@
     [ ./hardware-configuration.nix
       ../default.nix
       ../../modules/audio.nix
+      ../../modules/gnome.nix
     ];
 
   # Define the hosstname
@@ -43,36 +44,6 @@
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
   };
-
-  # Exclude unnecessary GNOME packages, maintain essential ones
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos
-    gnome-tour
-    cheese
-    gnome-terminal
-    gnome-user-docs
-    gnome-text-editor
-    gedit
-    epiphany
-    geary
-    totem
-    gnome-calendar
-  ]) ++ (with pkgs.gnome; [
-    gnome-music
-    tali # poker game
-    iagno # go game
-    hitori # sudoku game
-    atomix # puzzle game
-    gnome-weather
-    gnome-maps
-    gnome-clocks
-  ]);
-
-  services.xserver.excludePackages = [ pkgs.xterm ];
-
-  # Enable KDE Connect with GSConnect package
-    programs.kdeconnect.enable = true;
-    programs.kdeconnect.package = pkgs.gnomeExtensions.gsconnect;
 
   # State version setting
   system.stateVersion = "24.05"; # Keep this to match your initial install
---
+++ modules/gnome.nix
@@ -0,0 +1,31 @@
+{
+# Exclude unnecessary GNOME packages, maintain essential ones
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    cheese
+    gnome-terminal
+    gnome-user-docs
+    gnome-text-editor
+    gedit
+    epiphany
+    geary
+    totem
+    gnome-calendar
+  ]) ++ (with pkgs.gnome; [
+    gnome-music
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+    gnome-weather
+    gnome-maps
+    gnome-clocks
+  ]);
+
+  services.xserver.excludePackages = [ pkgs.xterm ];
+
+  # Enable KDE Connect with GSConnect package
+    programs.kdeconnect.enable = true;
+    programs.kdeconnect.package = pkgs.gnomeExtensions.gsconnect;
+}
