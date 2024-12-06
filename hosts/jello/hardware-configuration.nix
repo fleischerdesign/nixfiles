0595f20a17f21445d91a55e4512e6199f0f5204c
Philipp Fleischer <philipp@fleischer.design>
2024-12-06 03:12:07 +0100

    setup hardware configuration for new jello installation

--- hosts/jello/hardware-configuration.nix
+++ hosts/jello/hardware-configuration.nix
@@ -15,17 +15,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b0c5f380-de39-4138-aa2c-e71dc4c217f0";
+    { device = "/dev/disk/by-uuid/16819532-efa1-41c5-9a77-1aa4931d35e6";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/A59D-24BD";
+    { device = "/dev/disk/by-uuid/0AC6-F1D1";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/1fc49b41-b11f-4537-b208-897fae33e8c4"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -33,15 +35,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp7s0u1.useDHCP = lib.mkDefault true;
-
-  # Enable Bluetooth
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-
-  # Enable VAAPI
-  hardware.graphics.extraPackages = with pkgs; [ intel-vaapi-driver libvdpau-va-gl intel-media-driver ];
-  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+  # networking.interfaces.wlp9s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
