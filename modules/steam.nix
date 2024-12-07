0685d8204baeb5f8a31827fd4e3968f797da6b38
Philipp Fleischer <philipp@fleischer.design>
2024-12-07 07:50:43 +0100

    added sunshine

--- modules/steam.nix
+++ modules/steam.nix
@@ -7,7 +7,15 @@
     localNetworkGameTransfers.openFirewall = true;
   };
 
-environment.systemPackages = with pkgs; [
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+    
+  };
+
+  environment.systemPackages = with pkgs; [
     winetricks
     wine
     lutris
