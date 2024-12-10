22f75fea3c380da1299672da380327bc7bc3d6e4
Philipp Fleischer <philipp@fleischer.design>
2024-12-10 16:24:50 +0100

    changed secrets name

--- home-manager/philipp/home.nix
+++ home-manager/philipp/home.nix
@@ -23,7 +23,7 @@
     age.keyFile = "/home/philipp/.config/sops/age/key.txt"; # must have no password!
     # It's also possible to use a ssh key, but only when it has no password:
     #age.sshKeyPaths = [ "/home/user/path-to-ssh-key" ];
-    defaultSopsFile = ../../secrets/example.yaml;
+    defaultSopsFile = ../../secrets/main.yaml;
     secrets.openai = { };
     secrets.codestral = { };
     };
