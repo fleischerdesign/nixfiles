55072f66f92db50cae4ac19b1e6784d3db7e55b4
Philipp Fleischer <philipp@fleischer.design>
2025-04-03 00:03:17 +0200

    added gcloud

--- home-manager/philipp/packages.nix
+++ home-manager/philipp/packages.nix
@@ -22,6 +22,7 @@
     pkgs.moonlight-qt
     pkgs.rquickshare
     pkgs.freecad-wayland
+    pkgs.google-cloud-sdk
     (pkgs.callPackage ../../packages/lychee-slicer { })
     (pkgs.callPackage ../../packages/ficsit { })
   ];
