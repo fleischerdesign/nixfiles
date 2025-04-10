c5d1a407d911ab279b02a1fcdf5104ee8be3eee8
Philipp Fleischer <philipp@fleischer.design>
2025-04-11 01:36:18 +0200

    cartero added

--- home-manager/philipp/packages.nix
+++ home-manager/philipp/packages.nix
@@ -22,6 +22,7 @@
     pkgs.moonlight-qt
     pkgs.rquickshare
     pkgs.freecad-wayland
+    pkgs.cartero
     (pkgs.callPackage ../../packages/lychee-slicer { })
     (pkgs.callPackage ../../packages/ficsit { })
   ];
