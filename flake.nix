168115d733cfb3ea62714f948a3db3579d12032f
Philipp Fleischer <philipp@fleischer.design>
2025-08-13 16:48:39 +0200

    Changed unstable back to nixpkgs-unstable. removed permission for insecure libsoup package

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
