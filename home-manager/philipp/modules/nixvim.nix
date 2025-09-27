427a8a7fd8e5fb025ca56f1677c89d8890b674cc
Philipp Fleischer <philipp@fleischer.design>
2025-09-28 00:57:04 +0200

    feat(nixvim): add lsp, completion and telescope
    This commit significantly enhances the NixVim configuration by adding support for:
    
    - Language Server Protocol (LSP) via lspconfig with a wide range of language servers enabled.
    - Autocompletion using nvim-cmp.
    - Fuzzy finding with telescope.nvim for files, text and buffers.
    - The mapleader is now set to the space key.

--- home-manager/philipp/modules/nixvim.nix
+++ home-manager/philipp/modules/nixvim.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, config, ... }:
+{ lib, config, ... }:
 
 let
   cfg = config.my.homeManager.modules.nixvim;
@@ -10,6 +10,10 @@ in
     	defaultEditor = true;
     	viAlias = true;
     	vimAlias = true;
+
+		globals = {
+			mapleader = " ";
+		};
 
 		opts = {
             updatetime = 100;
@@ -24,9 +28,78 @@ in
 			shiftwidth = 4;
 			autoindent = true;
 		};
-		
+
+		lsp = {
+            servers = {
+              bashls.enable = true;
+			  ts_ls.enable = true;
+			  nil_ls.enable = true;
+			  html.enable = true;
+			  tailwindcss.enable = true;
+			  vue_ls.enable = true;
+			  cssls.enable = true;
+			  clangd.enable = true;
+			  rust_analyzer.enable = true;
+			  gopls.enable = true;
+			  marksman.enable = true;
+			  eslint.enable = true;
+			};
+		};
 		    colorschemes.catppuccin.enable = true;
-    		plugins.lualine.enable = true;
+		plugins = {
+			lspconfig.enable = true;
+			lualine.enable = true;
+			treesitter.enable = true;
+			web-devicons.enable = true;
+			telescope.enable = true;
+cmp = {
+	enable = true;
+  autoEnableSources = true;
+  settings = {
+  	sources = [
+    	{ name = "nvim_lsp"; }
+    	{ name = "path"; }
+    	{ name = "buffer"; }
+  	];
+    mapping = {
+      # Navigiere zum nächsten/vorherigen Eintrag
+      "<C-n>" = "cmp.mapping.select_next_item()";
+      "<C-p>" = "cmp.mapping.select_prev_item()";
+
+      # Schließe das Menü
+      "<C-e>" = "cmp.mapping.abort()";
+
+      # Bestätige die Auswahl mit Enter
+      "<CR>" = "cmp.mapping.confirm({ select = true })";
+
+      # Blättere durch die Dokumentation des Vorschlags
+      "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+      "<C-u>" = "cmp.mapping.scroll_docs(4)";
+    };
+	};
+  };
+		};
+
+		keymaps = [
+    {
+      mode = "n"; # Normal-Modus
+      key = "<leader>ff";
+      action = "<cmd>Telescope find_files<cr>";
+      options.desc = "Find Files"; # Beschreibung, die später angezeigt werden kann
+    }
+    {
+      mode = "n";
+      key = "<leader>fg";
+      action = "<cmd>Telescope live_grep<cr>";
+      options.desc = "Find Text (Live Grep)";
+    }
+    {
+      mode = "n";
+      key = "<leader>fb";
+      action = "<cmd>Telescope buffers<cr>";
+      options.desc = "Find Buffers";
+    }
+  ];
     };
   };
 }
