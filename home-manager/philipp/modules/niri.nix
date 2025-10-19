378789597474ef475545f132e3dbe29723098bf1
Philipp Fleischer <philipp@fleischer.design>
2025-10-19 22:34:30 +0200

    feat(niri): Complete and align keybindings with defaults
    - Add vim-style hjkl bindings for focus and movement.
    
    - Add bindings for first/last column focus and movement (Home/End).
    
    - Add alternative U/I bindings for workspace navigation.
    
    - Add consume/expel bindings for manual tiling (Mod+,/.).
    
    - Add binding to power-off monitors (Mod+Shift+P).
    
    This brings the custom configuration closer to the niri defaults and provides a more comprehensive set of controls.

--- home-manager/philipp/modules/niri.nix
+++ home-manager/philipp/modules/niri.nix
@@ -61,30 +61,60 @@
         "Mod+Right".action = focus-column-right;
         "Mod+Down".action = focus-window-down;
         "Mod+Up".action = focus-window-up;
+        "Mod+H".action = focus-column-left;
+        "Mod+L".action = focus-column-right;
+        "Mod+K".action = focus-window-up;
+        "Mod+J".action = focus-window-down;
 
         "Mod+Ctrl+Left".action = move-column-left;
         "Mod+Ctrl+Right".action = move-column-right;
         "Mod+Ctrl+Up".action = move-window-up;
         "Mod+Ctrl+Down".action = move-window-down;
+        "Mod+Ctrl+H".action = move-column-left;
+        "Mod+Ctrl+L".action = move-column-right;
+        "Mod+Ctrl+K".action = move-window-up;
+        "Mod+Ctrl+J".action = move-window-down;
+
+        "Mod+Home".action = focus-column-first;
+        "Mod+End".action = focus-column-last;
+        "Mod+Ctrl+Home".action = move-column-to-first;
+        "Mod+Ctrl+End".action = move-column-to-last;
+
+        "Mod+Comma".action = consume-window-into-column;
+        "Mod+Period".action = expel-window-from-column;
 
         "Mod+Shift+Left".action = focus-monitor-left;
         "Mod+Shift+Right".action = focus-monitor-right;
         "Mod+Shift+Down".action = focus-monitor-down;
         "Mod+Shift+Up".action = focus-monitor-up;
+        "Mod+Shift+H".action = focus-monitor-left;
+        "Mod+Shift+L".action = focus-monitor-right;
+        "Mod+Shift+K".action = focus-monitor-up;
+        "Mod+Shift+J".action = focus-monitor-down;
 
         "Mod+Shift+Ctrl+Left".action = move-column-to-monitor-left;
         "Mod+Shift+Ctrl+Right".action = move-column-to-monitor-right;
         "Mod+Shift+Ctrl+Up".action = move-column-to-monitor-up;
         "Mod+Shift+Ctrl+Down".action = move-column-to-monitor-down;
+        "Mod+Shift+Ctrl+H".action = move-column-to-monitor-left;
+        "Mod+Shift+Ctrl+L".action = move-column-to-monitor-right;
+        "Mod+Shift+Ctrl+K".action = move-column-to-monitor-up;
+        "Mod+Shift+Ctrl+J".action = move-column-to-monitor-down;
 
         "Mod+Page_Down".action = focus-workspace-down;
         "Mod+Page_Up".action = focus-workspace-up;
+        "Mod+U".action = focus-workspace-down;
+        "Mod+I".action = focus-workspace-up;
 
         "Mod+Ctrl+Page_Down".action = move-column-to-workspace-down;
         "Mod+Ctrl+Page_Up".action = move-column-to-workspace-up;
+        "Mod+Ctrl+U".action = move-column-to-workspace-down;
+        "Mod+Ctrl+I".action = move-column-to-workspace-up;
 
         "Mod+Shift+Page_Down".action = move-workspace-down;
         "Mod+Shift+Page_Up".action = move-workspace-up;
+        "Mod+Shift+U".action = move-workspace-down;
+        "Mod+Shift+I".action = move-workspace-up;
 
         "Mod+1".action = focus-workspace 1;
         "Mod+2".action = focus-workspace 2;
@@ -124,6 +154,7 @@
         "Ctrl+Print".action = screenshot;
         "Alt+Print".action = screenshot-window;
 
+        "Mod+Shift+P".action = power-off-monitors;
         "Mod+Shift+E".action = quit;
       };
     };
