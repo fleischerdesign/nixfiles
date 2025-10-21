ea8c32a6e75df5c4f27a0688daa5923b7c428c19
Philipp Fleischer <philipp@fleischer.design>
2025-10-21 10:12:56 +0200

    feat(niri): add xdg-portal and gnome-keyring
    This enables application launchers to discover desktop entries by configuring the xdg-desktop-portal with GTK and GNOME backends.
    
    It also enables the GNOME keyring service for secret storage.

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -30,5 +30,15 @@ in
     };
 
     services.upower.enable = true;
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-gnome
+      ];
+    };
+
+    services.gnome.gnome-keyring.enable = true;
   };
 }
