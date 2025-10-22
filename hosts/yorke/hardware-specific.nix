b4f63f714b71ab39df12dd586a54341fb030cc30
Philipp Fleischer <philipp@fleischer.design>
2025-10-22 13:09:59 +0200

    feat(yorke): enabled bluetooth

--- hosts/yorke/hardware-specific.nix
+++ hosts/yorke/hardware-specific.nix
@@ -3,4 +3,13 @@
 }:
 
 {
+hardware.bluetooth = {
+    enable = true;
+    # Empfohlen für BlueZ-Audio-Verbindungen
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
 }
