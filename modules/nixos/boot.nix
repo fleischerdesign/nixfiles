2d4cc2149a0ae939a3fe8b47c0ddaab85ccffb40
Philipp Fleischer <philipp@fleischer.design>
2025-11-02 16:00:29 +0100

    refactor(boot): migrate from systemd-boot to grub
    This commit replaces systemd-boot with GRUB as the default bootloader.
    
    GRUB is configured with 'useOSProber' to automatically detect other installed operating systems and adds the 'sleek-grub-theme' for a more visually appealing boot menu.

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,10 +1,31 @@
-{ config, lib, pkgs, ... }:
 {
-  # options.my.nixos.boot.enable = lib.mkEnableOption "Boot config";
-
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   config = lib.mkIf config.my.nixos.boot.enable {
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader = {
+      # GRUB für EFI-Systeme
+      grub = {
+        enable = true;
+        device = "nodev"; # Universell für alle EFI-Systeme
+        efiSupport = true;
+        useOSProber = true; # Findet andere OS automatisch
+
+        # Optional: Schöneres Theme
+        theme = pkgs.sleek-grub-theme;
+      };
+
+      efi = {
+        canTouchEfiVariables = true;
+      };
+
+      # Falls du systemd-boot deaktivieren willst
+      systemd-boot.enable = lib.mkForce false;
+    };
+
     boot.kernelParams = [ "quiet" ];
   };
 }
