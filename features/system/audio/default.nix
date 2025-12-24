9c2c054337066ca9e9030e789f7f297ec47beeeb
Philipp Fleischer <philipp@fleischer.design>
2025-12-24 21:16:32 +0100

    chore: updated flake.lock

--- features/system/audio/default.nix
+++ features/system/audio/default.nix
@@ -1,5 +1,10 @@
 # features/system/audio.nix
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.my.features.system.audio;
@@ -11,14 +16,21 @@ in
 
   config = lib.mkIf cfg.enable {
     services.pulseaudio.enable = false; # Disable PulseAudio to use Pipewire
-    security.rtkit.enable = true;       # Realtime scheduling for audio
+    security.rtkit.enable = true; # Realtime scheduling for audio
     services.pipewire = {
       enable = true;
-      alsa.enable = true;               # ALSA support
-      alsa.support32Bit = true;         # 32-bit ALSA applications
-      pulse.enable = true;              # PulseAudio compatibility
+      alsa.enable = true; # ALSA support
+      alsa.support32Bit = true; # 32-bit ALSA applications
+      pulse.enable = true; # PulseAudio compatibility
       # jack.enable = true;             # JACK compatibility, if needed
     };
+
+    services.pipewire.extraConfig.pipewire."99-min-quantum" = {
+      "context.properties" = {
+        "default.clock.min-quantum" = 1024;
+      };
+    };
+
     environment.systemPackages = with pkgs; [
       easyeffects # Audio effects processor
     ];
