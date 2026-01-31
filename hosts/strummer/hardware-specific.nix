65a05858005b7cdf90c39e5d280cbe208e4f9dfa
Philipp Fleischer <philipp@fleischer.design>
2026-01-31 23:55:08 +0100

    feat(strummer): mount additional storage drives to /data

--- hosts/strummer/hardware-specific.nix
+++ hosts/strummer/hardware-specific.nix
@@ -1,4 +1,16 @@
 { ... }:
 {
-  # Host-specific hardware tweaks
+  # 4 TB Storage
+  fileSystems."/data/storage" = {
+    device = "/dev/disk/by-uuid/7874b65e-816d-4377-9a8d-5c58fe2f65ca";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" ]; 
+  };
+
+  # 1 TB Storage (Sekundär)
+  fileSystems."/data/storage2" = {
+    device = "/dev/disk/by-uuid/ca58fccc-82f4-48bf-a9da-c83874a8b0a9";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" ]; 
+  };
 }
