8b63bdb46d56963e800adfa04895f15b5864c705
Philipp Fleischer <philipp@fleischer.design>
2026-02-01 03:28:02 +0100

    conf(strummer): enable cloudflare-dyndns in metadata

--- hosts/strummer/metadata.nix
+++ hosts/strummer/metadata.nix
@@ -7,6 +7,7 @@
     services.home-assistant.enable = true;
     services.esphome.enable = true;
     services.mealie.enable = true;
+    services.cloudflare-dyndns.enable = true;
     services.caddy.enable = true;
     services.authentik.outpost.proxy.enable = true;
     services.authentik.outpost.ldap.enable = true;
