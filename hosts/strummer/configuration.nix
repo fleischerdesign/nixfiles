b5c0b2f4be8f759a9bb6478b3f8597e868c2fe86
Philipp Fleischer <philipp@fleischer.design>
2026-02-01 01:08:36 +0100

    feat(caddy): add authentik forward_auth snippet and protect esphome

--- hosts/strummer/configuration.nix
+++ hosts/strummer/configuration.nix
@@ -21,9 +21,25 @@
     ];
   };
 
-  services.caddy.virtualHosts = {
-    "hass.fls.ancoris.ovh".extraConfig = "reverse_proxy 127.0.0.1:8123";
-    "esphome.fls.ancoris.ovh".extraConfig = "reverse_proxy 127.0.0.1:6052";
+  services.caddy = {
+    extraConfig = ''
+      (authentik) {
+        reverse_proxy /outpost.goauthentik.io/* 127.0.0.1:9000
+        forward_auth 127.0.0.1:9000 {
+          uri /outpost.goauthentik.io/auth/caddy
+          copy_headers X-Authentik-Username X-Authentik-Groups X-Authentik-Email X-Authentik-Name X-Authentik-Uid X-Authentik-Jwt X-Authentik-Meta-Jwks X-Authentik-Meta-Outpost X-Authentik-Meta-Provider X-Authentik-Meta-App X-Authentik-Meta-Version authorization
+          trusted_proxies private_ranges
+        }
+      }
+    '';
+
+    virtualHosts = {
+      "hass.fls.ancoris.ovh".extraConfig = "reverse_proxy 127.0.0.1:8123";
+      "esphome.fls.ancoris.ovh".extraConfig = ''
+        import authentik
+        reverse_proxy 127.0.0.1:6052
+      '';
+    };
   };
 
   # State version setting
