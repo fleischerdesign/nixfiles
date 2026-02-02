f429caa0ac21250b983e95fc76f51b54084bce83
Philipp Fleischer <philipp@fleischer.design>
2026-02-02 12:33:28 +0100

    fix(Jellyfin): Added intel-media-sdk to Jellyfin configuration

--- features/services/jellyfin/default.nix
+++ features/services/jellyfin/default.nix
@@ -36,6 +36,7 @@ in
     hardware.graphics = {
       enable = true;
       extraPackages = with pkgs; [
+	intel-media-sdk
         intel-media-driver # Modern Intel driver (Broadwell and newer)
         intel-vaapi-driver # Older Intel driver
         libvdpau-va-gl
