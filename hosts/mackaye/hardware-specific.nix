58f338709121457a3e3f1cf01efdc5f74b40d988
Philipp Fleischer <philipp@fleischer.design>
2026-02-03 18:53:56 +0100

    fix(mackaye): disable generic systemd-boot to allow grub

--- hosts/mackaye/hardware-specific.nix
+++ hosts/mackaye/hardware-specific.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
+  # Disable the generic bootloader feature from role/server (which enforces systemd-boot)
+  my.features.system.bootloader.enable = lib.mkForce false;
+
   # Bootloader Configuration
   # Most vServers use BIOS/MBR boot with GRUB.
   boot.loader.grub.enable = true;
