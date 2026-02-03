765ad54de58c57d4eca77023558283b46eb81b9a
Philipp Fleischer <philipp@fleischer.design>
2026-02-03 18:59:47 +0100

    fix(mackaye): force grub devices list to avoid duplication

--- hosts/mackaye/hardware-specific.nix
+++ hosts/mackaye/hardware-specific.nix
@@ -7,7 +7,7 @@
   # Bootloader Configuration
   # Most vServers use BIOS/MBR boot with GRUB.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda"; # Matches sda from lsblk
+  boot.loader.grub.devices = lib.mkForce [ "/dev/sda" ]; # Matches sda from lsblk
   
   # If UEFI is supported:
   # boot.loader.systemd-boot.enable = true;
