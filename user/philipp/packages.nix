afc1abe5b620ffc41a1eb42777d827fd60fe39b1
Philipp Fleischer <philipp@fleischer.design>
2026-02-08 13:56:14 +0100

    chore(user/philipp): replace geary with thunderbird

--- user/philipp/packages.nix
+++ user/philipp/packages.nix
@@ -10,37 +10,40 @@ let
   role = osConfig.my.role;
 in
 {
-  home.packages = with pkgs; [
-    # --- CLI / Server Safe ---
-    gemini-cli
-    yazi
-  ] ++ lib.optionals (role != "server") [
-    # --- Desktop Only ---
-    nerd-fonts.jetbrains-mono
-    # gimp
-    bitwarden-desktop
-    bitwarden-cli
-    geary
-    obsidian
-    orca-slicer
-    endeavour
-    resources
-    moonlight-qt
-    packet
-    freecad-wayland
-    yaak
-    penpot-desktop
-    deskflow
-    delfin
-    inkscape
-    evince
-    nautilus
-    gnome-disk-utility
-    bluetuith
-    (callPackage ../../packages/lychee-slicer { })
-    (callPackage ../../packages/ficsit { })
-    (callPackage ../../packages/karere { })
-  ];
+  home.packages =
+    with pkgs;
+    [
+      # --- CLI / Server Safe ---
+      gemini-cli
+      yazi
+    ]
+    ++ lib.optionals (role != "server") [
+      # --- Desktop Only ---
+      nerd-fonts.jetbrains-mono
+      # gimp
+      bitwarden-desktop
+      bitwarden-cli
+      thunderbird
+      obsidian
+      orca-slicer
+      endeavour
+      resources
+      moonlight-qt
+      packet
+      freecad-wayland
+      yaak
+      penpot-desktop
+      deskflow
+      delfin
+      inkscape
+      evince
+      nautilus
+      gnome-disk-utility
+      bluetuith
+      (callPackage ../../packages/lychee-slicer { })
+      (callPackage ../../packages/ficsit { })
+      (callPackage ../../packages/karere { })
+    ];
 
   programs.ghostty = lib.mkIf (role != "server") {
     enable = true;
