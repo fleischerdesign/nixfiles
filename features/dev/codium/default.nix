0ebe9645d5e1d8df159e1710f14b648b3a34c5b8
Philipp Fleischer <philipp@fleischer.design>
2026-03-04 11:54:22 +0100

    change(codium): Removed some vscode extensions i dont longer use

--- features/dev/codium/default.nix
+++ features/dev/codium/default.nix
@@ -1,5 +1,10 @@
 # features/dev/codium.nix
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.my.features.dev.codium;
@@ -10,75 +15,75 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home-manager.sharedModules = [{
-      programs.vscode = {
-        enable = true;
-        package = pkgs.vscodium;
-        mutableExtensionsDir = false;
+    home-manager.sharedModules = [
+      {
+        programs.vscode = {
+          enable = true;
+          package = pkgs.vscodium;
+          mutableExtensionsDir = false;
 
-        profiles.default = {
-          extensions = with pkgs.vscode-marketplace; [
-            rooveterinaryinc.roo-cline
-            prisma.prisma
-            bradlc.vscode-tailwindcss
-            vue.volar
-            mkhl.direnv
-            ms-vscode.cpptools
-            jnoortheen.nix-ide
-            dart-code.flutter
-            dart-code.dart-code
-            redhat.java
-            vscjava.vscode-java-debug
-            vscjava.vscode-java-test
-            vscjava.vscode-java-dependency
-            visualstudioexptteam.vscodeintellicode
-            dbaeumer.vscode-eslint
-            bbenoist.qml
-            cweijan.vscode-database-client2
-          ];
-          userSettings = {
-            "extensions.autoUpdate" = false;
-            "git.confirmSync" = false;
-            "git.autofetch" = true;
-            "terminal.integrated.fontWeight" = "normal";
+          profiles.default = {
+            extensions = with pkgs.vscode-marketplace; [
+              bradlc.vscode-tailwindcss
+              vue.volar
+              mkhl.direnv
+              ms-vscode.cpptools
+              jnoortheen.nix-ide
+              redhat.java
+              vscjava.vscode-java-debug
+              vscjava.vscode-java-test
+              vscjava.vscode-java-dependency
+              vscjava.vscode-maven
+              vscjava.vscode-gradle
+              dbaeumer.vscode-eslint
+              bbenoist.qml
+              svelte.svelte-vscode
+              rust-lang.rust-analyzer
+            ];
+            userSettings = {
+              "extensions.autoUpdate" = false;
+              "git.confirmSync" = false;
+              "git.autofetch" = true;
+              "terminal.integrated.fontWeight" = "normal";
 
-            "terminal.integrated.persistentSessionReviveProcess" = "never";
+              "terminal.integrated.persistentSessionReviveProcess" = "never";
 
-            "update.mode" = "none";
+              "update.mode" = "none";
 
-            "window.titleBarStyle" = "custom";
-            "window.customTitleBarVisibility" = "auto";
+              "window.titleBarStyle" = "custom";
+              "window.customTitleBarVisibility" = "auto";
 
-            "security.workspace.trust.enabled" = true;
+              "security.workspace.trust.enabled" = true;
 
-            "editor.fontFamily" = "'JetBrainsMono Nerd Font', 'monospace', monospace";
-            "editor.fontLigatures" = true;
-            "editor.bracketPairColorization.enabled" = true;
-            "editor.guides.bracketPairs" = "active";
-            "editor.minimap.enabled" = false;
-            "editor.smoothScrolling" = true;
-            "terminal.integrated.smoothScrolling" = true;
-            "explorer.confirmDragAndDrop" = false;
-            "explorer.confirmDelete" = false;
+              "editor.fontFamily" = "'JetBrainsMono Nerd Font', 'monospace', monospace";
+              "editor.fontLigatures" = true;
+              "editor.bracketPairColorization.enabled" = true;
+              "editor.guides.bracketPairs" = "active";
+              "editor.minimap.enabled" = false;
+              "editor.smoothScrolling" = true;
+              "terminal.integrated.smoothScrolling" = true;
+              "explorer.confirmDragAndDrop" = false;
+              "explorer.confirmDelete" = false;
 
-            "C_Cpp.default.compilerPath" = "gcc";
+              "C_Cpp.default.compilerPath" = "gcc";
 
-            "direnv.restart.automatic" = true;
+              "direnv.restart.automatic" = true;
 
-            "nix.enableLanguageServer" = true;
-            "nix.serverPath" = "nil";
-            "nix.formatterPath" = "nixfmt";
-            "dart.renameFilesWithClasses" = "prompt";
-            "dart.previewFlutterUiGuides" = true;
+              "nix.enableLanguageServer" = true;
+              "nix.serverPath" = "nil";
+              "nix.formatterPath" = "nixfmt";
+              "dart.renameFilesWithClasses" = "prompt";
+              "dart.previewFlutterUiGuides" = true;
 
-            "chat.agent.enabled" = true;
+              "chat.agent.enabled" = true;
 
-            "redhat.telemetry.enabled" = false;
+              "redhat.telemetry.enabled" = false;
 
-            "editor.formatOnSave" = true;
+              "editor.formatOnSave" = true;
+            };
           };
         };
-      };
-    }];
+      }
+    ];
   };
 }
