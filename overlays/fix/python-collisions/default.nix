bfbc132d8fe21722f9c410d0f92e998812e7555e
Philipp Fleischer <philipp@fleischer.design>
2026-04-30 04:37:07 +0200

    fix(python): aggressively disable tests for moto and websockets

--- overlays/fix/python-collisions/default.nix
+++ overlays/fix/python-collisions/default.nix
@@ -3,19 +3,26 @@ final: prev: {
     (python-final: python-prev: {
       django-tenants = python-prev.django-tenants.overrideAttrs (old: {
         postInstall = (old.postInstall or "") + ''
-          rm -rf $out/${prev.python3.sitePackages}/docs || true
+          rm -rf $out/${python-prev.python.sitePackages}/docs || true
         '';
       });
       cryptography = python-prev.cryptography.overrideAttrs (old: {
         postInstall = (old.postInstall or "") + ''
-          rm -rf $out/${prev.python3.sitePackages}/docs || true
+          rm -rf $out/${python-prev.python.sitePackages}/docs || true
         '';
       });
       moto = python-prev.moto.overrideAttrs (old: {
         doCheck = false;
+        dontCheck = true;
+        pytestCheckPhase = "true";
+        checkPhase = "true";
       });
       websockets = python-prev.websockets.overrideAttrs (old: {
         doCheck = false;
+        dontCheck = true;
+        pytestCheckPhase = "true";
+        unittestCheckPhase = "true";
+        checkPhase = "true";
       });
     })
   ];
