141186af91618096ad1e98b4a99c592ebe741291
Philipp Fleischer <philipp@fleischer.design>
2026-07-21 10:56:38 +0200

    feat(hermes-desktop): switch to native Rust compilation via buildRustPackage

--- packages/custom/hermes-desktop/default.nix
+++ packages/custom/hermes-desktop/default.nix
@@ -1,75 +1,54 @@
 # packages/custom/hermes-desktop/default.nix
-# Declarative AppImage wrapper for Hermes WebUI Desktop (Tauri 2)
+# Native Rust compilation for Hermes WebUI Desktop (Tauri 2)
 {
   lib,
-  appimageTools,
-  fetchurl,
-  ...
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  wrapGAppsHook3,
+  webkitgtk_4_1,
+  gtk3,
+  libsoup_3,
+  openssl,
+  dbus,
+  librsvg,
+  glib,
 }:
 
 let
   manifest = builtins.fromJSON (builtins.readFile ./manifest.json);
   pname = manifest.name;
-  inherit (manifest) version hash;
-
-  src = fetchurl {
-    url = "https://github.com/${manifest.upstream.owner}/${manifest.upstream.repo}/releases/download/v${version}/${manifest.upstream.assetName}";
-    inherit hash;
-  };
-
-  appimageContents = appimageTools.extractType2 {
-    inherit pname version src;
-  };
+  inherit (manifest) version srcHash cargoHash;
 in
-appimageTools.wrapType2 {
-  inherit pname version src;
+rustPlatform.buildRustPackage {
+  inherit pname version cargoHash;
 
-  extraBwrapArgs = [
-    "--chdir"
-    "/"
+  src = fetchFromGitHub {
+    owner = manifest.upstream.owner;
+    repo = manifest.upstream.repo;
+    rev = "v${version}";
+    hash = srcHash;
+  };
+
+  sourceRoot = "source/src-tauri";
+
+  nativeBuildInputs = [
+    pkg-config
+    wrapGAppsHook3
+  ];
+  buildInputs = [
+    webkitgtk_4_1
+    gtk3
+    libsoup_3
+    openssl
+    dbus
+    librsvg
+    glib
   ];
 
-  extraPkgs =
-    pkgs: with pkgs; [
-      webkitgtk_4_1
-      gtk3
-      glib
-      gdk-pixbuf
-      cairo
-      pango
-      atk
-      libsoup_3
-      openssl
-      dbus
-      libxkbcommon
-      wayland
-      libx11
-      libxcomposite
-      libxdamage
-      libxext
-      libxfixes
-      libxrandr
-      libglvnd
-      libGL
-      mesa
-      vulkan-loader
-      librsvg
-    ];
-
-  extraProfile = ''
-    export PWD="$HOME"
-    cd "$HOME"
-    export HERMES_DESKTOP_SAFE_RENDER=1
-    export WEBKIT_DISABLE_DMABUF_RENDERER=1
-  '';
-
-  extraInstallPhase = ''
-    install -m 444 -D ${appimageContents}/Hermes.WebUI.Desktop.desktop $out/share/applications/hermes-desktop.desktop || true
-    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/Hermes.WebUI.Desktop.png $out/share/icons/hicolor/512x512/apps/hermes-desktop.png || true
-
-    substituteInPlace $out/share/applications/hermes-desktop.desktop \
-      --replace-fail 'Exec=Hermes.WebUI.Desktop' "Exec=$out/bin/hermes-desktop" \
-      --replace-fail 'Icon=Hermes.WebUI.Desktop' "Icon=hermes-desktop" || true
+  postInstall = ''
+    ln -s $out/bin/hermes-webui-desktop $out/bin/hermes-desktop
+    install -m 444 -D ../src-tauri/icons/icon.png $out/share/icons/hicolor/512x512/apps/hermes-desktop.png || true
   '';
 
   meta = with lib; {
@@ -77,6 +56,6 @@ appimageTools.wrapType2 {
     homepage = "https://github.com/hermes-webui/hermes-desktop-rust";
     license = licenses.mit;
     platforms = [ "x86_64-linux" ];
-    mainProgram = "hermes-desktop";
+    mainProgram = "hermes-webui-desktop";
   };
 }
